8be6c6a742ce9100e842497df747e15bc0963503
ukpagrace <ukpagraceu@gmail.com>
2026-04-02 15:02:48 +0100

    added argocd.yml, app.js and deploy.yml

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from Kubernetes on VM2!\n');
+  res.end('Hello from Kubernetes on VM2 hope it works this time!\n');
 }).listen(3000, '0.0.0.0');
 console.log('Server running on port 3000');
