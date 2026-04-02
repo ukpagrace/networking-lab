1d1ef46f8c97240337bb772e6997ceff51ab5ed3
ukpagrace <ukpagraceu@gmail.com>
2026-04-02 13:32:18 +0100

    update response message

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from VM2 private server!\n');
+  res.end('Hello from Kubernetes on VM2!\n');
 }).listen(3000, '0.0.0.0');
 console.log('Server running on port 3000');
